488e795ac021efb486e0868d86d6cf694f353fe9
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-27 17:37:22 +0800

    Update

---
+++ Test71/MyFrame.java
@@ -0,0 +1,58 @@
+package Test71;
+
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MyFrame extends JFrame implements ActionListener{
+
+    JMenuItem loadItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(new FlowLayout());
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
+        JMenu helpMenu = new JMenu("Help");
+
+        loadItem = new JMenuItem("Load");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        loadItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.add(loadItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(helpMenu);
+
+        this.setJMenuBar(menuBar);
+
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        if(e.getSource() == loadItem) {
+            System.out.println("brrrrrrrrrrrr file loaded");
+        }
+        if(e.getSource() == saveItem) {
+            System.out.println("file saved");
+        }
+        if(e.getSource() == exitItem) {
+            System.out.println("byeeee!");
+        }
+    }
+    
+}
