c3fa58436d637887faefb97d1b29226a75c2a48f
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-27 17:06:31 +0800

    Update

--- Test70/Main.java
+++ Test70/Main.java
@@ -2,6 +2,8 @@ package Test70;
 
 public class Main {
     public static void main(String[] args) {
-        
+        // progress bar
+
+        ProgressBarDemo demo = new ProgressBarDemo();
     }
 }
