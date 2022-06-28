059f2874862c42c793fc8c50b521a0ef06a4a7a0
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-28 16:39:46 +0800

    Update

---
+++ Test72/Main.java
@@ -0,0 +1,12 @@
+package Test72;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // JFileChooser
+
+        new MyFrame();
+        
+    }
+}
---
+++ Test72/MyFrame.java
@@ -0,0 +1,21 @@
+package Test72;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.event.*;
+import java.awt.FlowLayout;
+
+public class MyFrame extends JFrame implements ActionListener{
+
+    JButton button;
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton();
+        button.addActionListener(this);
+
+
+    
+}
