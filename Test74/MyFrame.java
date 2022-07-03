2902942358afbd46cb9455e2a3047df01d2c54e7
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-07-03 23:04:15 +0800

    Update MyFrame.java

--- Test74/MyFrame.java
+++ Test74/MyFrame.java
@@ -1,5 +1,29 @@
-package Test74;
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class MyFrame {
-    
+public class MyFrame extends JFrame implements KeyListener{
+
+	
+	JLabel label;
+	ImageIcon icon;
+	
+	MyFrame(){		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500,500);
+		this.setLayout(null);
+		this.addKeyListener(this);
+		
+		icon = new ImageIcon("rocket.png");
+		
+		label = new JLabel();
+		label.setBounds(0, 0, 100, 100);
+		label.setIcon(icon);
+		//label.setBackground(Color.red);
+		//label.setOpaque(true);
+		this.getContentPane().setBackground(Color.black);
+		this.add(label);
+		this.setVisible(true);
+	}
+
 }
